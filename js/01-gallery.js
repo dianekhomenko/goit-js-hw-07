c126f91467cf395b043442930f62723f8b05ffcd
htmldiana <dianekhomenko@gmail.com>
2023-02-07 19:06:49 +0100

    task-01 fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ const gallery =
 const template = galleryItems
   .map(
     (item) =>
-      `<li><img class="gallery__image" src="${item.preview}" alt="${item.description}" data-url="${item.original}"></li>`
+      `<li><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" /></a></li>`
   )
   .join("");
 
@@ -15,18 +15,30 @@ gallery.insertAdjacentHTML("beforeend", template);
 
 gallery.addEventListener("click", onClick);
 
+let galleryBig;;
+
 function onClick(evt) {
   if (evt.target.nodeName !== "IMG") {
     return;
   }
-  let bigPhoto = evt.target.dataset.url;
-  console.log(bigPhoto);
+  let bigPhoto = evt.target.dataset.source;
 
-  basicLightbox
+  galleryBig = basicLightbox
     .create(
       `
 		<img width="1400" height="900" src="${bigPhoto}">
 	`
     )
     .show();
+}
+
+document.addEventListener("keydown", closeGallery);
+
+function closeGallery({ key }) {
+
+    if (galleryBig.show || key === "Escape") {
+        console.log("close");
+        galleryBig.close();
+    }   
+    
 }
