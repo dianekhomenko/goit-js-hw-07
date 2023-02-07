e2693003e8bec0c6644716b01cf2c4411d2c80e3
htmldiana <dianekhomenko@gmail.com>
2023-02-07 18:18:45 +0100

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery =
+  document.querySelector(".gallery");
+
+const template = galleryItems
+  .map(
+    (item) =>
+      `<li><img class="gallery__image" src="${item.preview}" alt="${item.description}" data-url="${item.original}"></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", template);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  let bigPhoto = evt.target.dataset.url;
+  console.log(bigPhoto);
+
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${bigPhoto}">
+	`
+    )
+    .show();
+}
